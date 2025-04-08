2859dc1018ffd50cab2009045b51337caabdd637
Imanuel <charliechatur@gmail.com>
2025-04-08 17:37:53 -0400

    Added classes to be implemented

---
+++ main.py
@@ -0,0 +1,23 @@
+import random
+import time
+
+
+def list_animals_imanuel():
+    animal_list = []
+    return animal_list
+
+
+def most_hungry_imanuel():
+    pass
+
+
+def most_amount_fed_imanuel():
+    pass
+
+
+def main():
+    pass
+
+
+if "__main__" == __name__:
+    main()
