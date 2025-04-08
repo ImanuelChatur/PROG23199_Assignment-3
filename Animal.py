19ef5ee75616b4483a99c41ae759c2a8912b7527
Imanuel <charliechatur@gmail.com>
2025-04-08 15:47:57 -0400

    Animal class Created

---
+++ Animal.py
@@ -0,0 +1,15 @@
+class Animal:
+    def __init__(self, name, required_food):
+        self.name = name
+        self.required_food = 0
+        self.hungry_count = 0
+        self.food_consumed = 0
+
+    def set_feeding_count(self, feeding_count):
+        pass
+
+    def set_hungry_count(self):
+        pass
+
+    def calculate_food_consumed(self):
+        pass
